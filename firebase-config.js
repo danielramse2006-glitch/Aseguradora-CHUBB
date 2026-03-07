23db27029a422384618ef0686f3f390c77e3d822
danielramse2006-glitch <danielramse2006@gmail.com>
2026-03-07 15:58:58 -0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,19 @@
 // ============================================================
-//  CONFIGURACIÓN FIREBASE — reemplaza con tus datos reales
+//  CONFIGURACIÓN FIREBASE
 // ============================================================
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCtIagFFJBFRjvg5usXTm575YqOeeDE1G0",
-  authDomain: "mi-inventario-51f82.firebaseapp.com",
-  projectId: "mi-inventario-51f82",
-  storageBucket: "mi-inventario-51f82.firebasestorage.app",
-  messagingSenderId: "794197755416",
-  appId: "1:794197755416:web:e1bbab46cda2bdbb5da56d",
-  measurementId: "G-P1PRV5HE93"
+  apiKey: "AIzaSyCp3Ii_M2E8MPuj0yqLSY6YEtssmKc6aDo",
+  authDomain: "seguros-polizas.firebaseapp.com",
+  projectId: "seguros-polizas",
+  storageBucket: "seguros-polizas.firebasestorage.app",
+  messagingSenderId: "258311010009",
+  appId: "1:258311010009:web:d699d0d220be11535fb0c8"
 };
+
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
